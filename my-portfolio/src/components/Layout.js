3de873cc552659c6a030e36c1edd596bedfb2076
jonathanreyes16 <jonathan.reyes16@tectijuana.edu.mx>
2023-05-09 19:18:02 -0700

    header,footer and styles

--- my-portfolio/src/components/Layout.js
+++ my-portfolio/src/components/Layout.js
@@ -1,11 +1,17 @@
 import { Container } from "@mui/material";
 import Header from "./Header";
 import Footer from "./Footer";
+import Link from "next/link";
 
 export default function Layout ({children}) {
 return (
     <>
-    <Header></Header>
+    <Header title="jonathan">
+        <Link href="/">Home</Link> 
+        <Link href="/about">About</Link>
+        <Link href="/project">Project</Link>
+        <Link href="/contact">Contact</Link>
+    </Header>
     <Container fixed>
         <main>{children}</main>
     </Container>
